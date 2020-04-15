39ae051e3ec9a677a67fbfaa9f033af1d75acd3f
josdem <joseluis.delacruz@gmail.com>
2020-04-15 18:23:14 -0400

    Setting first emitter processor test

---
+++ src/test/java/com/jos/dem/reactive/spring/workshop/EmitterProcessorTest.java
@@ -0,0 +1,32 @@
+package com.jos.dem.reactive.spring.workshop;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.EmitterProcessor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.test.StepVerifier;
+
+@SpringBootTest
+class EmitterProcessorTest {
+
+  @Test
+  @DisplayName("should create an emitter processor")
+  void shouldGenerateEmitterProcessor() {
+    EmitterProcessor<String> processor = EmitterProcessor.create();
+    produce(processor.sink());
+    consume(processor);
+  }
+
+  private void produce(FluxSink<String> sink) {
+    sink.next("1");
+    sink.next("2");
+    sink.next("3");
+    sink.complete();
+  }
+
+  private void consume(Flux<String> publisher) {
+    StepVerifier.create(publisher).expectNext("1").expectNext("2").expectNext("3").verifyComplete();
+  }
+}
