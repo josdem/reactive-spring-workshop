0591144a9981602576bd1b80cdaaf1daa7b2d2f7
josdem <joseluis.delacruz@gmail.com>
2020-04-15 16:44:35 -0400

    Starting new Reactive Spring Workshop Application

---
+++ src/main/java/com/jos/dem/reactive/spring/workshop/SpringReactiveWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.jos.dem.reactive.spring.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveWorkshopApplication.class, args);
+	}
+
+}
