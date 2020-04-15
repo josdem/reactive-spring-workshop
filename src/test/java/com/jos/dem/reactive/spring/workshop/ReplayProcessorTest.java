b169f89805dfdcc76b015e02559a1d26e6d261bb
josdem <joseluis.delacruz@gmail.com>
2020-04-15 19:52:08 -0400

    Adding replay processor test

---
+++ src/test/java/com/jos/dem/reactive/spring/workshop/ReplayProcessorTest.java
@@ -0,0 +1,36 @@
+package com.jos.dem.reactive.spring.workshop;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.ReplayProcessor;
+import reactor.test.StepVerifier;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+class ReplayProcessorTest {
+
+  @Test
+  @DisplayName("should replay processor")
+  void shouldReplayProcessor() {
+    ReplayProcessor<String> processor = ReplayProcessor.create(2, false);
+    produce(processor.sink());
+    consume(processor);
+  }
+
+  private void produce(FluxSink<String> sink) {
+    sink.next("1");
+    sink.next("2");
+    sink.next("3");
+    sink.complete();
+  }
+
+  private void consume(Flux<String> publisher) {
+    IntStream.range(0, 5)
+        .forEach(
+            it -> StepVerifier.create(publisher).expectNext("2").expectNext("3").verifyComplete());
+  }
+}
