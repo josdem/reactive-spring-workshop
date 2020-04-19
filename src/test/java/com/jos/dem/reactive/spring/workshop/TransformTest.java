00c96a8b2d73aee452378f8700508b595b8b9f59
josdem <joseluis.delacruz@gmail.com>
2020-04-19 17:17:57 -0400

    Adding transform test

---
+++ src/test/java/com/jos/dem/reactive/spring/workshop/TransformTest.java
@@ -0,0 +1,25 @@
+package com.jos.dem.reactive.spring.workshop;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TransformTest {
+
+  @Test
+  @DisplayName("Transform operator")
+  void shouldTransform() {
+    var finished = new AtomicBoolean();
+    var letters =
+        Flux.just("A", "B", "C")
+            .transform(stringFlux -> stringFlux.doFinally(signal -> finished.set(true)));
+    StepVerifier.create(letters).expectNextCount(3).verifyComplete();
+
+    assertTrue(finished.get(), "Finished boolean should be true");
+  }
+}
