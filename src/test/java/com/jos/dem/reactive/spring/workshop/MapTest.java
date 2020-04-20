7214f786a00957360807f892dec78ec0f248616c
josdem <joseluis.delacruz@gmail.com>
2020-04-20 12:03:23 -0400

    Adding map test

---
+++ src/test/java/com/jos/dem/reactive/spring/workshop/MapTest.java
@@ -0,0 +1,16 @@
+package com.jos.dem.reactive.spring.workshop;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class MapTest {
+
+  @Test
+  @DisplayName("testing Map on Flux")
+  void shouldTestMap() {
+    var data = Flux.just("a", "b", "c").map(String::toUpperCase);
+    StepVerifier.create(data).expectNext("A", "B", "C").verifyComplete();
+  }
+}
