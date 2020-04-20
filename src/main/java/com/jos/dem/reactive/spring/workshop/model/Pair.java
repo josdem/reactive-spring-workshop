31e2075f61c748614f5f4941757b8c939fa33b4e
josdem <joseluis.delacruz@gmail.com>
2020-04-20 15:26:53 -0400

    Adding flat map test

---
+++ src/main/java/com/jos/dem/reactive/spring/workshop/model/Pair.java
@@ -0,0 +1,11 @@
+package com.jos.dem.reactive.spring.workshop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Pair {
+  private int id;
+  private long delay;
+}
