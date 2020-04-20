c51e99356f41737500c4cab70a8292375358e47b
josdem <joseluis.delacruz@gmail.com>
2020-04-20 16:01:40 -0400

    Adding flat map test example

---
+++ src/test/java/com/jos/dem/reactive/spring/workshop/FlatMapTest.java
@@ -0,0 +1,25 @@
+package com.jos.dem.reactive.spring.workshop;
+
+import com.jos.dem.reactive.spring.workshop.model.Pair;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class FlatMapTest {
+
+  @Test
+  @DisplayName("flat map test")
+  void shouldFlatMap() {
+    Flux<Integer> data =
+        Flux.just(new Pair(1, 300), new Pair(2, 200), new Pair(3, 100))
+            .flatMap(pair -> this.delayReplyFor(pair.getId(), pair.getDelay()));
+    StepVerifier.create(data).expectNext(3, 2, 1).verifyComplete();
+  }
+
+  private Flux<Integer> delayReplyFor(Integer i, long delay) {
+    return Flux.just(i).delayElements(Duration.ofMillis(delay));
+  }
+}
