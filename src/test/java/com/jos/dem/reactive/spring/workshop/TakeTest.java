68e08f7ce59b3ecac4c3a3d7c1c5f381ee7e952f
josdem <joseluis.delacruz@gmail.com>
2020-04-21 18:59:55 -0400

    Refactor extract method

--- src/test/java/com/jos/dem/reactive/spring/workshop/TakeTest.java
+++ src/test/java/com/jos/dem/reactive/spring/workshop/TakeTest.java
@@ -11,7 +11,7 @@ class TakeTest {
   @DisplayName("take test examples")
   void shouldTakeUntilCount() {
     var count = 10;
-    Flux<Integer> take = Flux.range(0, 1000).take(count);
+    Flux<Integer> take = getRange().take(count);
     StepVerifier.create(take).expectNextCount(10).verifyComplete();
   }
 
@@ -19,7 +19,11 @@ class TakeTest {
   @DisplayName("take until test example")
   void shouldTakeUntil() {
     var count = 50;
-    Flux<Integer> take = Flux.range(0, 1000).takeUntil(i -> i == (count - 1));
+    Flux<Integer> take = getRange().takeUntil(i -> i == (count - 1));
     StepVerifier.create(take).expectNextCount(count).verifyComplete();
+  }
+
+  private Flux<Integer> getRange() {
+    return Flux.range(0, 1000);
   }
 }
