703b607e7e6c07d3add3d8474a6e35a0453e8bde
josdem <joseluis.delacruz@gmail.com>
2020-04-21 19:25:27 -0400

    Adding filter test

---
+++ src/test/java/com/jos/dem/reactive/spring/workshop/FilterTest.java
@@ -0,0 +1,17 @@
+package com.jos.dem.reactive.spring.workshop;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class FilterTest {
+
+  @Test
+  @DisplayName("filter example")
+  void shouldFilter() {
+    Flux<Integer> range = Flux.range(0, 1000).take(5);
+    Flux<Integer> filter = range.filter(i -> i % 2 == 0);
+    StepVerifier.create(filter).expectNext(0, 2, 4).verifyComplete();
+  }
+}
