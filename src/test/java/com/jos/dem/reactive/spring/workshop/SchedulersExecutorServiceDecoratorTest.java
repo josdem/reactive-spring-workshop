49238788d17e53e09ad93db6eae0bc58dfc8630b
josdem <joseluis.delacruz@gmail.com>
2020-05-03 11:34:37 -0400

    Completing test case

--- src/test/java/com/jos/dem/reactive/spring/workshop/SchedulersExecutorServiceDecoratorTest.java
+++ src/test/java/com/jos/dem/reactive/spring/workshop/SchedulersExecutorServiceDecoratorTest.java
@@ -1,14 +1,22 @@
 package com.jos.dem.reactive.spring.workshop;
 
 import org.aopalliance.intercept.MethodInterceptor;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aop.framework.ProxyFactoryBean;
+import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
 
+import java.time.Duration;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SchedulersExecutorServiceDecoratorTest {
 
@@ -22,6 +30,18 @@ class SchedulersExecutorServiceDecoratorTest {
     Schedulers.resetFactory();
     Schedulers.addExecutorServiceDecorator(
         this.rsb, (scheduler, scheduledExecutorService) -> this.decorate(scheduledExecutorService));
+  }
+
+  @Test
+  @DisplayName("Change default decorator")
+  void shouldChangeDefaultDecorator() {
+    Flux<Integer> integerFlux = Flux.just(1).delayElements(Duration.ofMillis(1));
+    StepVerifier.create(integerFlux)
+        .thenAwait(Duration.ofMillis(10))
+        .expectNextCount(1)
+        .verifyComplete();
+
+    assertEquals(1, methodInvocationCounts.get(), "should have one invocation");
   }
 
   private ScheduledExecutorService decorate(ScheduledExecutorService executorService) {
@@ -43,5 +63,11 @@ class SchedulersExecutorServiceDecoratorTest {
       log.error(cnfe.getMessage());
     }
     return null;
+  }
+
+  @AfterEach
+  void after() {
+    Schedulers.resetFactory();
+    Schedulers.removeExecutorServiceDecorator(rsb);
   }
 }
