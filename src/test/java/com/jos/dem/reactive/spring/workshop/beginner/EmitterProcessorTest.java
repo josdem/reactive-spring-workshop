791ea2d064fa6d8db0c7b8a1938ef2c8186f8681
josdem <joseluis.delacruz@gmail.com>
2020-12-01 16:59:12 -0600

    Adding emitter processor consumer

--- src/test/java/com/jos/dem/reactive/spring/workshop/beginner/EmitterProcessorTest.java
+++ src/test/java/com/jos/dem/reactive/spring/workshop/beginner/EmitterProcessorTest.java
@@ -2,11 +2,16 @@ package com.jos.dem.reactive.spring.workshop.beginner;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.FluxSink;
 import reactor.test.StepVerifier;
 
+
 class EmitterProcessorTest {
+
+    private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Test
     @DisplayName("should create an emitter processor")
@@ -14,6 +19,14 @@ class EmitterProcessorTest {
         EmitterProcessor<Integer> processor = EmitterProcessor.create();
         produce(processor.sink());
         StepVerifier.create(processor).expectNext(1).expectNext(2).expectNext(3).verifyComplete();
+    }
+
+    @Test
+    @DisplayName("should consume processor values")
+    void shouldConsumeProcessorValues() {
+        EmitterProcessor<Integer> processor = EmitterProcessor.create();
+        produce(processor.sink());
+        processor.subscribe(value -> log.info("value: " + value));
     }
 
     private void produce(FluxSink<Integer> sink) {
