96a5a98022ef2ec13bcae56f42bcf6bb9318b34b
josdem <joseluis.delacruz@gmail.com>
2020-12-27 20:28:08 -0600

    Adding some do on next

--- src/test/java/com/jos/dem/reactive/spring/workshop/beginner/MonoTest.java
+++ src/test/java/com/jos/dem/reactive/spring/workshop/beginner/MonoTest.java
@@ -2,6 +2,8 @@ package com.jos.dem.reactive.spring.workshop.beginner;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -9,11 +11,17 @@ import java.time.Duration;
 
 class MonoTest {
 
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Test
     @DisplayName("Mono test")
     void shouldDelayElements(){
         Mono<String> result = Mono.just("Hello Bummer!")
-                .delayElement(Duration.ofSeconds(1));
+                .delayElement(Duration.ofSeconds(1))
+                .doOnNext(it -> log.info("hello world!"))
+                .delayElement(Duration.ofSeconds(1))
+                .doOnNext(it -> log.info("hello josdem!"))
+                .doOnTerminate(() -> log.info("Complete!"));
         StepVerifier.create(result).expectNext("Hello Bummer!").verifyComplete();
     }
 }
